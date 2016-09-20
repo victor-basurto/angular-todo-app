d235435b27ec9bb6bcc140c1fcb467cd2a387d27
victor basurto <my.mvplace@gmail.com>
2016-09-20 12:57:57 -0700

    007- changed design on todos

--- public/javascripts/services/todos.fac.js
+++ public/javascripts/services/todos.fac.js
@@ -1,48 +1,39 @@
 (function() {
 	'use strict';
-	TodoApp.factory('Todos', [ '$http', '$mdToast', function( $http, $mdToast ) {
+	TodoApp.factory('Todos', [ '$http', function( $http ) {
 
+		/**
+		 * [Get data from DB]
+		 * @return {Data} [get data from DB]
+		 */
 		function get() {
 			return $http.get( '/api/todos' );	
 		}
 
+		/**
+		 * [Create new TODO]
+		 * @param  {Object} todoData [current value]
+		 * @return {Post}          [post object]
+		 */
 		function create( todoData ) {
 			return $http.post( '/api/todos', todoData );
 		}
 
+		/**
+		 * [Delete a TODO]
+		 * @param  {Key} id [item to be deleted]
+		 * @return {Delete}    [item deleted]
+		 */
 		function deleteTodoData( id ) {
 			return $http.delete( '/api/todos/' + id );
-		}
-
-		function showToast( content, delay ) {
-			$mdToast.show(
-				$mdToast.simple()
-					.textContent( content )
-					.position( 'top right' )
-					.hideDelay( delay )
-			);
 		}
 
 		return {
 			get: get,
 			create: create,
-			deleteTodoData: deleteTodoData,
-			showToast: showToast
+			deleteTodoData: deleteTodoData
 		}
 
 		
 	}]);
 })();
-
-
-// return  {
-// 	get: function() {
-// 		return $http.get( '/api/todos' );
-// 	},
-// 	create: function( todoData ) {
-// 		return $http.post( '/api/todos', todoData );
-// 	},
-// 	delete: function( id ) {
-// 		return $http.delete( '/api/todos/' + id );
-// 	}
-// }
