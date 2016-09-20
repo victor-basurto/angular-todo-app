4053674bc6d6069bed8477763aa047b20ee144c1
victor basurto <my.mvplace@gmail.com>
2016-09-20 13:56:53 -0700

    008- todo-form directive `widget`

---
+++ public/javascripts/directives/todo-form.dir.js
@@ -0,0 +1,25 @@
+'use strict';
+
+TodoApp.directive('todoForm', [function() {
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		scope: {
+			formDataText: '=',
+			create: '&'
+		}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'AE',
+		// template: '',
+		templateUrl: 'templates/directives/todo-form.dir.html',
+		replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			
+		}
+	};
+}]);
